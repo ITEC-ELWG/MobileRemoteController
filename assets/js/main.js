528b685451ac875e8e7614eb3ba63df11e0153f9
RayTaylorLin <raytaylorlin@gmail.com>
2015-12-04 16:36:57 +0800

    添加smartJQueryZoom插件并启用缩放图片功能 k# 	new file:   assets/js/main.js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+(function() {
+    $(document).ready(function() {
+        $('img').smartZoom({
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '60%',
+            containerBackground: "#eee"
+        });
+    });
+})();
