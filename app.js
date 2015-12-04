546193a6ec74b6a988cc50313e8411a20d61ad68
RayTaylorLin <raytaylorlin@gmail.com>
2015-12-04 15:46:26 +0800

    搭建基本骨架k

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+app.use('/assets', express.static('assets'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/assets/index.html');
+});
+
+
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
