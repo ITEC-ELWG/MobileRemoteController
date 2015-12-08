db607c0d8cb671bd43d3c3ba1a584ba9ec9f9257
RayTaylorLin <raytaylorlin@gmail.com>
2015-12-08 15:25:23 +0800

    使用Openseadragon显示多张图片，并做最基本的master样式区分

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,61 @@
 (function() {
     $(document).ready(function() {
-        $('img').smartZoom({
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '60%',
-            containerBackground: "#eee"
+        var isMasterMode = window.location.hash === '#master';
+        var imageUrls = [
+            '/assets/images/test1.jpg',
+            '/assets/images/test2.jpg',
+            '/assets/images/test3.jpg'
+        ];
+        var viewer = initOpenSeadragon(imageUrls, isMasterMode);
+
+        var socket = io.connect('http://localhost:3000');
+        socket.on('connect', function() {
+            socket.emit('SLAVE_AUTH', {
+                key: 123
+            });
         });
+
+        socket.on('MID_MASTER_COMMAND', function(data) {
+            console.log(data);
+            executeCommand(data);
+        });
+
+
+        function initOpenSeadragon(imageUrls, isMasterMode) {
+            var tileSources = $.map(imageUrls, function(url, i) {
+                return {
+                    type: 'image',
+                    url: url
+                };
+            });
+            return OpenSeadragon({
+                id: 'openseadragon',
+                // prefixUrl: '/assets/images/',
+                tileSources: tileSources,
+                zoomPerClick: 1.0,
+                showNavigationControl: false,
+                showSequenceControl: false,
+                sequenceMode: true,
+                showReferenceStrip: isMasterMode,
+
+                visibilityRatio: 0.4,
+                blendTime: 0.5,
+                // 最大缩放比例，大于1可失真放大
+                maxZoomPixelRatio: 1.3,
+            });
+        }
+
+        function executeCommand(data) {
+            switch (data.command) {
+                case 'prev':
+                    $('.left.carousel-control').click();
+                    break;
+                case 'next':
+                    $('.right.carousel-control').click();
+                    break;
+                default:
+                    break;
+            }
+        }
     });
 })();
